f468c973675250ce575d46a4a701ce64954deea0
Jason <66013853+ahmola@users.noreply.github.com>
2025-04-30 18:11:27 +0900

    init

---
+++ backend/beverage/src/main/java/com/example/beverage/BeverageApplication.java
@@ -0,0 +1,13 @@
+package com.example.beverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageApplication.class, args);
+	}
+
+}
