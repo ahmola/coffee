cc729b651b6f2a0620ea90bae6ccf4461cc6ce13
Jason <66013853+ahmola@users.noreply.github.com>
2025-05-01 18:45:47 +0900

    discovery gateway

---
+++ backend/config/Readme.md
@@ -0,0 +1,6 @@
+# Config Server
+
+### Reference
+
+* [Spring Docs](https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_spring_cloud_config_server)
+* [Spring Guides](https://spring.io/guides/gs/centralized-configuration)
---
+++ backend/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// jwt 인증과 보안을 추가해야하긴함
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
